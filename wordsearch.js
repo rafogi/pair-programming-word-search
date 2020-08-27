da94edd6862be2f203fcb36ff216c70af3ae36ad
Raphael Cordero <raffycordero2001@yahoo.ca>
2020-08-27 00:36:11 +0000

    modified wordsearch.js to take vertical and horizontal words of the 2nd argument and return true of false if found

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+// Put your solution here
+  let newAr = [];
+  for (let i = 0; i < matrix.length; ++i) {
+    for (let j = 0; j < matrix[i].length; ++j) {
+      if (matrix[i][j] === undefined) continue;
+      if (newAr[j] === undefined) newAr[j] = [];
+      // swap the x and y coords for the newAr
+      newAr[j][i] = matrix[i][j];
     }
-}
+  }
+  return newAr;
+};
 
-module.exports = wordSearch
+
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  let x = transpose(letters);
+  const verticalJoin = x.map(a => a.join(''));
+  for (let y of verticalJoin) {
+    if (y.includes(word)) return true;
+  }
+  return false;
+};
+
+
+
+module.exports = wordSearch;
